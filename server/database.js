e7151ad181884fec5ad6193cc68a596258aa9a27
bulbul-akabulbul <galsol02@gmail.com>
2020-08-29 10:48:35 +0300

    Added dotenv

--- server/database.js
+++ server/database.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'db_socialnetwork',
-    password: process.env.DBPASS,
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
