7544bceaf0c3a7dc204ba9db97d3db9d30354719
Bulbul Akabulbul <galsol02@gmail.com>
2020-09-05 20:56:55 +0300

    Authentication process is now asynchronous

--- server/passport.js
+++ server/passport.js
@@ -28,27 +28,52 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser(async (id, done) => {
-  done(null, await db.getUserById(id));
+passport.deserializeUser((id, done) => {
+  db.getUserById(id)
+    .then((user) => {
+      done(null, { ...user, isAdmin: user.roleId <= db.roles.ADMIN });
+    })
+    .catch((err) => done(err, false));
 });
 
 /**
  * @callback getAllowedID
  * @param {Object} req The node request object associated with that request.
  * @returns {number} An ID to be compared against the logged in user ID.
+ *
+ * @callback asyncGetAllowedID
+ * @param {Object} req The node request object associated with that request.
+ * @returns {Promise<number>} Promise returning ID to be compared against the logged in user ID.
  */
 
 /**
  * Checks for authentciation. If the user isn't authenticated he is redirected to the LOGIN_ROUTE
  * If any of the optional parameters isn't matched, the user is return 403 "Forbidden".
  * @param {boolean} [administrative = true] If true then users with higher than or equal to ADMIN are permited.
- * @param {getAllowedID} [getAllowedID = undefined] Callback that returns the ID that should be compared against the current logged in user.
+ * @param {getAllowedID|asyncGetAllowedID} [getAllowedID = undefined] Callback that returns the ID that should be compared against the current logged in user.
  */
 exports.requiresAuthentication = (administrative = false, getAllowedID = undefined) => (req, res, next) => {
   if (req.isUnauthenticated()) return res.redirect(LOGIN_ROUTE);
-  if ((administrative && req.user.roleId <= db.roles.ADMIN) || (getAllowedID && req.user.id == getAllowedID(req)))
+
+  check = () => {
+    if (!((administrative && req.user.isAdmin) || (getAllowedID && req.user.id == allowedId))) {
+      console.log(allowedId);
+      return res.status(403).send("Forbidden");
+    }
+    console.log("4");
     return next();
-  return res.status(403).send("Forbidden");
+  };
+
+  // getAllowedID can now by either async or sync function, to allow use of database queries.
+  let allowedId = getAllowedID(req);
+  if (allowedId instanceof Promise)
+    allowedId
+      .then((id) => {
+        allowedId = id;
+        check();
+      })
+      .catch((err) => res.status(500).send("Oops! Seems like something went wrong."));
+  else check();
 };
 
 /**
